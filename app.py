21b91930bd2decbbcc451d64cfc69f9626d13522
cml22 <106069931+cml22@users.noreply.github.com>
2024-10-15 08:13:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ from bs4 import BeautifulSoup
 from docx import Document
 from docx.shared import RGBColor
 from docx.oxml import parse_xml
-from docx.oxml.ns import nsdecls
 
-def create_word_file(filename, content, url):
+def create_word_file(filename, content):
     document = Document()
 
     # Titre du document
@@ -21,20 +20,22 @@ def create_word_file(filename, content, url):
     document.save(filename)
 
 def add_hyperlink(paragraph, url, text):
+    # Add the hyperlink run
     run = paragraph.add_run(text)
     run.font.color.rgb = RGBColor(0, 0, 255)  # Set hyperlink color to blue
     run.font.underline = True  # Underline for hyperlink
 
-    # Add the hyperlink
+    # Add the hyperlink relationship
     r_id = paragraph.part.rels.add_relationship(
         'http://schemas.openxmlformats.org/officeDocument/2006/relationships/hyperlink',
         url,
         'hyperlink',
         target_mode='External'
     )
-    
-    # Set hyperlink properties
+
+    # Set hyperlink properties in the XML
     hyperlink = parse_xml(r'<w:hyperlink r:id="{}" xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main">'.format(r_id))
+    paragraph._element.append(hyperlink)
     run._element.get_or_add_rPr().append(parse_xml(r'<w:rStyle w:val="Hyperlink"/>'))
 
 def extract_content(url):
@@ -58,5 +59,5 @@ jira_input = st.text_input('Add the JIRA link (TT - Traffic Team)')
 if st.button('Create Word File'):
     content = extract_content(url_input)
     filename = 'extracted_content.docx'
-    create_word_file(filename, content, url_input)
+    create_word_file(filename, content)
     st.success(f'Word file created: {filename}')
